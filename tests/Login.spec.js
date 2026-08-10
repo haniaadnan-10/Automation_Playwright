f645faf26b19e8e76b4f4314f91931e42c1a7be7
haniaadnan-10 <hania.adnan2000@gmail.com>
2026-08-10 17:58:19 +0500

    "Data-Driven"

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,55 +1,17 @@
 import {test, expect} from '@playwright/test';
+import LoginData from '../testdata/loginData.json';
 
-//Valid username and valid password
-test('Login Test Case', async ({page}) => {
+LoginData.ValidUsers.forEach((data) => {
+
+   //Valid username and valid password
+    test(`Login Test Case ${data.username}`, async ({page}) => {
+
     await page.goto('https://www.saucedemo.com/');
-    await page.fill('#user-name', 'standard_user');
-    await page.fill('#password', 'secret_sauce');
+    await page.fill('#user-name', data.username);
+    await page.fill('#password', data.password);
     await page.click('#login-button');
     await expect(page.locator('//*[@id="header_container"]/div[2]/span')).toHaveText("Products");
     
-});
+}); 
 
-test('Login Test Case with valid credentials', async ({page}) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.fill('#user-name', 'problem_user');
-    await page.fill('#password', 'secret_sauce');
-    await page.click('#login-button');
-    await expect(page.locator('//*[@id="header_container"]/div[2]/span')).toHaveText("Products");
-});
-
-//invalid username and valid password
-test('Login Test Case With Invalid Username & valid password', async ({page}) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.fill('#user-name', 'abs');
-    await page.fill('#password', 'secret_sauce');
-    await page.click('#login-button');
-    await expect(page.locator('//*[@id="login_button_container"]/div/form/div[3]/h3')).toHaveText("Epic sadface: Username and password do not match any user in this service");
-});
-
-//Valid username and invalid passwoerd
-test('Login Test Case with valid username & invalid password', async ({page}) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.fill('#user-name', 'standard_user');
-    await page.fill('#password', 'abc');
-    await page.click('#login-button');
-    await expect(page.locator('//*[@id="login_button_container"]/div/form/div[3]/h3')).toHaveText("Epic sadface: Username and password do not match any user in this service");
-});
-
-//Invalid username and invalid password
-test('Login test case with invalid username & password', async ({page}) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.fill('#user-name', 'hania');
-    await page.fill('#password', '12345');
-    await page.click('#login-button');
-    await expect(page.locator('//*[@id="login_button_container"]/div/form/div[3]/h3')).toHaveText("Epic sadface: Username and password do not match any user in this service");
-});
-
-//Empty username and empty password
-test('Login test case with empty username & password', async ({page}) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.fill('#user-name', '');
-    await page.fill('#password', '');
-    await page.click('#login-button');
-    await expect(page.locator('//*[@id="login_button_container"]/div/form/div[3]/h3')).toHaveText("Epic sadface: Username is required");
 });
