b346fb50695890807144aa4ed88170c3c547a8c8
haniaadnan-10 <hania.adnan2000@gmail.com>
2026-08-19 23:39:03 +0500

    "Allure Reports configured"

--- Pages/loginPage.js
+++ Pages/loginPage.js
@@ -1,3 +1,5 @@
+import {test} from '@playwright/test';
+
 class loginPage{
 
     constructor(page)
@@ -10,15 +12,32 @@ class loginPage{
 
     }
 
+    async attachScreenshot(name){
+        await test.info().attach(name, {
+            body: await this.page.screenshot(),
+            contentType: 'image/png',
+        });
+    }
+
     async gotoURL(){
         await this.page.goto('https://www.saucedemo.com/');
+        await this.attachScreenshot('01 - Login Page Opened');
     }
+    
 
     async Login(username, password){
         await this.username.fill(username);
+        await this.attachScreenshot('02 - Enter Username');
+
         await this.password.fill(password);
+        await this.attachScreenshot('02 - Enter Password');
+
         await this.LoginButton.click();
+        await this.attachScreenshot('03 - Click Login');
+        
     }
+
+    
 }
 
 export default loginPage;
--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -3,18 +3,29 @@ import loginData from '../testdata/loginData.json';
 import loginPage from '../Pages/loginPage';
 import logout from '../Pages/logout';
 
-
+test.describe('Login with valid credentials', ()=> {
    //Valid username and valid password
     test(`Login Test Case`, async ({page}) => {
 
     const login = new loginPage(page);
     const data = loginData.ValidUsers[0];
 
-    await login.gotoURL();
-    await login.Login(data.username, data.password);
-    await expect((login.message)).toHaveText(data.ExpectedMsg);
+    await test.step('Open Login Page', async() => {
+        await login.gotoURL();
+    });
 
-    const logOut = new logout(page);
-    await logOut.logout();
+    await test.step('Enter credentials and Login', async() => {
+    await login.Login(data.username, data.password);
+    });
+
+    await test.step('Welcome Verfiy Message', async() => {
+    await expect((login.message)).toHaveText(data.ExpectedMsg);
+    await login.attachScreenshot('05 - Welcome msg should be displayed')
+    });
+    
+
+    //const logOut = new logout(page);
+    //await logOut.logout();
     
 }); 
+});
